f7b437693759de9c0d2b76a98efa1b0bbef4840c
Kyle G <garnerk10@gmail.comgit config --global user.email garnerk10@gmail.com>
2022-10-13 08:31:18 -0400

    fixed expanding lists

--- GamePage.js
+++ GamePage.js
@@ -1,7 +1,7 @@
 //Game night page
 //collapsing game list
 
-let listNo = document.getElementById('listNo');
+/*let listNo = document.getElementById('listNo');
 let buttonNum = document.getElementById('buttonNum');
 
 const collapse = () => {
@@ -12,4 +12,18 @@ const collapse = () => {
     }
 };
 
-buttonNum.addEventListener('click', collapse);
+buttonNum.addEventListener('click', collapse);*/
+
+let allLists = document.getElementsByTagName('h3');
+
+for (let i = 0; i < allLists.length; i++){
+    allLists[i].onclick = function() {
+        if(this.parentNode){
+            let childList = this.parentNode.getElementsByTagName('ul');
+
+            if(childList[i].style.display == 'block'){
+                childList[i].style.display = 'none'
+            } else {childList[i].style.display = 'block'}
+        }
+    }
+};
